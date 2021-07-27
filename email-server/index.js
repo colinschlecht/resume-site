70e304f329bd38320328f7cd1e92633a3d54abd5
Colin Schlecht <schlechtcolin@yahoo.com>
2021-07-26 19:05:19 -0700

    adds test-email server

---
+++ email-server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodeMailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+
+const port = 4444;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.listen(port, () => {
+	console.log("We are live on port 4444");
+});
+
+app.get("/", (req, res) => {
+	res.send("Welcome to my api");
+});
+
+app.post("/api/email", (req, res) => {
+	const data = req.body;
+
+	const smtpTransport = nodeMailer.createTransport({
+		service: "Gmail",
+		port: 465,
+		auth: {
+			user: "schlechtcolin1@gmail.com",
+			pass: PASSWORD,
+		},
+	});
+	const mailOptions = {
+		from: data.email,
+		to: "schlechtcolin1@gmail.com",
+		subject: "Email from personal site",
+		html: `<p>Name: ${data.name}</p>
+                <p>Email: ${data.email}</p>
+                <p>Message: ${data.message}</p>`,
+	};
+
+	smtpTransport.sendMail(mailOptions, (error, response) => {
+		if (error) {
+			res.send(error);
+		} else {
+			res.send("Success");
+		}
+		smtpTransport.close();
+	});
+});
--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -1,101 +1,115 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { Form, Button, Message } from "semantic-ui-react";
 
 const Contact = () => {
-  const [loading, setLoading] = useState(false);
-  const [emailAddress, setEmailAddress] = useState("");
-  const [name, setName] = useState("");
-  const [msg, setMsg] = useState("");
-  const [emailError, setEmailError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [emailAddress, setEmailAddress] = useState("");
+	const [name, setName] = useState("");
+	const [msg, setMsg] = useState("");
+	const [emailError, setEmailError] = useState(false);
+	const [sendError, setSendError] = useState(false);
 
-  const disableSubmit = () => !emailAddress || !name || !msg;
+	const disableSubmit = () => !emailAddress || !name || !msg;
 
-  const validateEmail = (addr) => {
-    const re =
-      //eslint-disable-next-line
-      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(String(addr).toLowerCase());
-  };
-  const handleSubmit = (e) => {
-    setLoading(true);
-    e.preventDefault();
-    if (!validateEmail(emailAddress)) {
-      setEmailError(true);
-      setLoading(false);
-    } else {
-      console.log("trying to email");
-      console.log(loading);
-      setTimeout(() => {
-        console.log("sent");
-        setName("");
-        setEmailAddress("");
-        setMsg("");
-        setLoading(false);
-      }, 1000);
-    }
-  };
+	const validateEmail = (addr) => {
+		const re =
+			//eslint-disable-next-line
+			/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+		return re.test(String(addr).toLowerCase());
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+    setSendError(false)
+		let data = {
+			name: name,
+			email: emailAddress,
+			message: msg,
+		};
+		if (!validateEmail(emailAddress)) {
+			setEmailError(true);
+		} else {
+			axios
+				.post("http://localhost:4444/email", data)
+				.then((res) => {
+					console.log(res.data);
+					setName("");
+					setEmailAddress("");
+					setMsg("");
+					setLoading(false);
+				})
+				.catch(() => {
+					console.log("Message not sent");
+          setSendError(true)
+					setLoading(false);
+          setName("");
+					setEmailAddress("");
+				});
+		}
+	};
+	return (
+		<>
+			<div
+				className="ui raised very padded text container segment"
+				id="main-content4"
+			>
+				<h1 id="contact-title-test">Contact</h1>
 
-  console.log("hi");
-  return (
-    <>
-      <div
-        className="ui raised very padded text container segment"
-        id="main-content4"
-      >
-        <h1 id="contact-title-test">Contact</h1>
+				<div className="contact-form form-container">
+					{sendError && (
+						<Message>
+							Thank you for your interest in contacting me. There was an error
+							in sending your email, please manually email me at:
+							schlechtcolin1@gmail.com
+						</Message>
+					)}
+					<Form>
+						<Form.Input
+							fluid
+							label="Name"
+							placeholder="Name"
+							id="form-input-first-name"
+							className="contact-input"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+						<Form.Input
+							error={emailError}
+							fluid
+							label="Email Address"
+							placeholder="email@email.com"
+							id="form-input-email"
+							className="contact-input"
+							value={emailAddress}
+							onChange={(e) => {
+								if (emailError) {
+									setEmailError(false);
+								}
+								setEmailAddress(e.target.value);
+							}}
+						/>
 
-        <div className="contact-form form-container">
-          <Message>
-            Thank you for your interest in contacting me. This is a placeholder
-            form while I work on getting a mail service set up. For the
-            meantime, please manually email me at: schlechtcolin1@gmail.com
-          </Message>
-          <Form>
-            <Form.Input
-              fluid
-              label="Name"
-              placeholder="Name"
-              id="form-input-first-name"
-              className="contact-input"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <Form.Input
-              error={emailError}
-              fluid
-              label="Email Address"
-              placeholder="email@email.com"
-              id="form-input-email"
-              className="contact-input"
-              value={emailAddress}
-              onChange={(e) => {
-                if (emailError) {
-                  setEmailError(false);
-                }
-                setEmailAddress(e.target.value);
-              }}
-            />
+						<Form.TextArea
+							id="form-input-message"
+							className="contact-input"
+							label="Message"
+							placeholder="..."
+							value={msg}
+							onChange={(e) => setMsg(e.target.value)}
+						/>
 
-            <Form.TextArea
-              id="form-input-message"
-              className="contact-input"
-              label="Message"
-              placeholder="..."
-              value={msg}
-              onChange={(e) => setMsg(e.target.value)}
-            />
-
-            <Button
-              loading={loading}
-              disabled={disableSubmit()}
-              onClick={(e) => handleSubmit(e)}
-            >
-              Submit
-            </Button>
-          </Form>
-        </div>
-      </div>
-    </>
-  );
+						<Button
+							loading={loading}
+							disabled={disableSubmit()}
+							onClick={(e) => handleSubmit(e)}
+						>
+							Submit
+						</Button>
+					</Form>
+				</div>
+			</div>
+		</>
+	);
 };
 export default Contact;
