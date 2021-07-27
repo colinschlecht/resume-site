cf2cfd14fe2104429cc93d287002cf909545504e
Colin Schlecht <schlechtcolin@yahoo.com>
2021-07-26 23:54:19 -0700

    removes commentd out text

--- api/email.js
+++ api/email.js
@@ -25,42 +25,12 @@ export default function sendEmail(req, res) {
 
 	return smtpTransport.sendMail(mailOptions, (error, response) => {
 		if (error) {
-      console.log(error)
+			// console.log(error);
 			res.send(error);
 		} else {
-      console.log("success")
+			// console.log("success");
 			res.send("Success");
 		}
 		smtpTransport.close();
 	});
 }
-
-// export default function post(req, res) {
-// 	const data = req.body;
-
-// 	const smtpTransport = nodemailer.createTransport({
-// 		host: "smtp.mailtrap.io",
-// 		port: 2525,
-// 		auth: {
-// 			user: "edce0adb730a18",
-// 			pass: "7446d2dc264881",
-// 		},
-// 	});
-// 	const mailOptions = {
-// 		from: data.email,
-// 		to: "schlechtcolin1@gmail.com",
-// 		subject: "Email from personal site",
-// 		html: `<p>Name: ${data.name}</p>
-//                   <p>Email: ${data.email}</p>
-//                   <p>Message: ${data.message}</p>`,
-// 	};
-
-// 	smtpTransport.sendMail(mailOptions, (error, response) => {
-// 		if (error) {
-// 			res.send(error);
-// 		} else {
-// 			res.send("Success");
-// 		}
-// 		smtpTransport.close();
-// 	});
-// }
